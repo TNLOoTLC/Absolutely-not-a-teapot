3cf7859779ccf15125ed9f99c34829ee68a7b254
0x5c <0x5c.dev@gmail.com>
2018-11-01 04:23:56 -0400

    Humour upgrade! Better look! Wow!

--- Absolutely not a teapot.php
+++ Absolutely not a teapot.php
@@ -2,6 +2,18 @@
 header("HTTP/1.1 418 I'm a teapot");
 ?>
 <html>
-<h1>418 I'm a teapot</h1><br>
-<p>The HTCPCP Server is a teapot. The responding entity MAY be short and stout.</p>
+    <head>
+        <style type="text/css">
+            body {
+            margin-top: 24px;
+            margin-bottom: 24px;
+            margin-right: 24px;
+            margin-left: 24px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>418 I'm a teapot</h1><br>
+        <p><i>What? You actually expected me to brew coffee?</i></p>
+    </body>
 </html>
